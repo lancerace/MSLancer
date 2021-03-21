3223708704457de7938f3ef3ddd08dc4823a270e
lancerace <lancerace@hotmail.com>
2021-03-22 01:40:59 +0800

    fix resuable block map script

--- scripts/portal/block_map.js
+++ scripts/portal/block_map.js
@@ -1,8 +1,8 @@
 function enter(pi) {
-    if (pi.getPlayer().getLevel() < 30) {
-        pi.playerMessage(5, "[SeaMS] This portal is blocked as part of features.");
-    } else {
-        var map = pi.getPlayer().getClient().getChannelServer().getMapFactory().getMap(whatevermapiditwarpedtobefore);
-         pi.getPlayer().changeMap(map, 0);
-    }
+    ////if (pi.getPlayer().getLevel() < 30) {
+        pi.playerMessage(5, "[SeaMS] This portal is blocked as part of featuress.");
+    //} else {
+    //    var map = pi.getPlayer().getClient().getChannelServer().getMapFactory().getMap(whatevermapiditwarpedtobefore);
+     //    pi.getPlayer().changeMap(map, 0);
+   // }
 } 
