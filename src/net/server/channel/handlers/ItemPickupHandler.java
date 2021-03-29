65a80ca46148e46381afc1fdbed1c56001f1469b
lancerace <lancerace@hotmail.com>
2021-03-29 19:57:26 +0800

    up thresold for vac hack coordinate

--- src/net/server/channel/handlers/ItemPickupHandler.java
+++ src/net/server/channel/handlers/ItemPickupHandler.java
@@ -50,7 +50,7 @@ public final class ItemPickupHandler extends AbstractMaplePacketHandler {
         
         Point charPos = chr.getPosition();
         Point obPos = ob.getPosition();
-        if (Math.abs(charPos.getX() - obPos.getX()) > 400 || Math.abs(charPos.getY() - obPos.getY()) > 400) {
+        if (Math.abs(charPos.getX() - obPos.getX()) > 700 || Math.abs(charPos.getY() - obPos.getY()) > 700) {
             AutobanFactory.ITEM_VAC.alert(chr,"Trying to be a vaccum cleaner");
             FilePrinter.printError(FilePrinter.EXPLOITS + c.getPlayer().getName() + ".txt", c.getPlayer().getName() + " tried to pick up an item too far away. Mapid: " + chr.getMapId() + " Player pos: " + charPos + " Object pos: " + obPos);
             return;
